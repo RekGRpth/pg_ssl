6c1065cf9e2f25ab24eaee03eed434b365761713
RekGRpth <rekgrpth@gmail.com>
2019-05-15 16:49:48 +0500

    up

--- pg_ssl.c
+++ pg_ssl.c
@@ -1,37 +1,17 @@
 #include <postgres.h>
 #include <fmgr.h>
 
-#include <catalog/pg_type.h>
-#include <openssl/err.h>
 #include <openssl/pem.h>
-#include <openssl/pkcs7.h>
 #include <utils/builtins.h>
 
 #define EXTENSION(function) Datum (function)(PG_FUNCTION_ARGS); PG_FUNCTION_INFO_V1(function); Datum (function)(PG_FUNCTION_ARGS)
 
 PG_MODULE_MAGIC;
 
-static inline int B64_write_ASN1(BIO *out, ASN1_VALUE *val, BIO *in, int flags, const ASN1_ITEM *it) {
-    BIO *b64;
-    int r;
-    b64 = BIO_new(BIO_f_base64());
-    if (b64 == NULL) {
-        ASN1err(ASN1_F_B64_WRITE_ASN1, ERR_R_MALLOC_FAILURE);
-        return 0;
-    }
-    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-    out = BIO_push(b64, out);
-    r = i2d_ASN1_bio_stream(out, val, in, flags, it);
-    (void)BIO_flush(out);
-    BIO_pop(out);
-    BIO_free(b64);
-    return r;
-}
-
 EXTENSION(sign) {
     int flags = PKCS7_TEXT;
     char *cert, *data, *str, *pstr;
-    BIO *in, *out, *tbio;
+    BIO *in, *out, *out2, *tbio, *b64;
     X509 *scert;
     EVP_PKEY *skey;
     PKCS7 *p7;
@@ -46,7 +26,13 @@ EXTENSION(sign) {
     if (!(skey = PEM_read_bio_PrivateKey(tbio, NULL, 0, NULL))) ereport(ERROR, (errmsg("!skey")));
     if (!(p7 = PKCS7_sign(scert, skey, NULL, in, flags))) ereport(ERROR, (errmsg("!p7")));
     if (!(out = BIO_new(BIO_s_mem()))) ereport(ERROR, (errmsg("!out")));
-    if (!B64_write_ASN1(out, (ASN1_VALUE *)p7, in, flags, ASN1_ITEM_rptr(PKCS7))) ereport(ERROR, (errmsg("!B64_write_ASN1")));
+    if (!(b64 = BIO_new(BIO_f_base64()))) ereport(ERROR, (errmsg("!b64")));
+    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+    out2 = BIO_push(b64, out);
+    if (!i2d_ASN1_bio_stream(out2, (ASN1_VALUE *)p7, in, flags, ASN1_ITEM_rptr(PKCS7))) ereport(ERROR, (errmsg("!i2d_ASN1_bio_stream")));
+    (void)BIO_flush(out2);
+    BIO_pop(out2);
+    BIO_free(b64);
     (long)BIO_get_mem_data(out, &str);
     pstr = pstrdup(str);
     PKCS7_free(p7);
